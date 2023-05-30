e3be0797be4b25040ce419e93b1af6529f7f341b
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2023-05-30 01:13:53 -0600

    Added a wryna command

---
+++ src/commands/wryna.ts
@@ -0,0 +1,40 @@
+import {
+  ApplicationCommandOptionTypes,
+  ApplicationCommandTypes,
+  InteractionResponseTypes,
+} from "../../deps.ts";
+import {
+  humanizeMilliseconds,
+  snowflakeToTimestamp,
+} from "../utils/helpers.ts";
+import { createCommand } from "./mod.ts";
+
+createCommand({
+  name: "wryna",
+  description: "What was your nickname in highschool?",
+  type: ApplicationCommandTypes.ChatInput,
+  scope: "Global",
+  options: [
+    {
+      type: ApplicationCommandOptionTypes.String,
+      name: "input",
+      description: "Text you would like to send to this command.",
+      required: true,
+    },
+  ],
+  execute: async (bot, interaction) => {
+    const input = interaction.data?.options?.find(
+      (option) => option.name === "input"
+    );
+    await bot.helpers.sendInteractionResponse(
+      interaction.id,
+      interaction.token,
+      {
+        type: InteractionResponseTypes.ChannelMessageWithSource,
+        data: {
+          content: `${input?.value} was ${interaction.user.username}'s nickname in highschool.`,
+        },
+      }
+    );
+  },
+});
