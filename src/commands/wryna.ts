8c97e6c0dc96768e510ee3f451ab3b13b94f473a
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2023-06-05 22:03:21 -0600

    changes to wryna commands

--- src/commands/wryna.ts
+++ src/commands/wryna.ts
@@ -19,7 +19,7 @@ export class UserCommand extends Command {
 
 	// Message command
 	public async messageRun(message: Message, args: Args) {
-		return this.sendPing(message, args.getOption('nickname') || 'NOTHING');
+		return this.sendPing(message, args.getOption('nickname') || message.content.split('!wryna ')[1]);
 	}
 
 	// Chat Input (slash) command
@@ -32,9 +32,11 @@ export class UserCommand extends Command {
 		nickname: string
 	) {
 		interactionOrMessage instanceof Message
-			? await interactionOrMessage.channel.send({ content: `${nickname} was ${interactionOrMessage.author.tag}'s nickname in highschool!` })
+			? await interactionOrMessage.channel.send({
+					content: `${nickname} was ${interactionOrMessage.author.username}'s nickname in highschool!`
+			  })
 			: await interactionOrMessage.reply({
-					content: `${nickname} was ${interactionOrMessage.user.tag}'s nickname in highschool!`,
+					content: `${nickname} was ${interactionOrMessage.user.username}'s nickname in highschool!`,
 					fetchReply: true
 			  });
 	}
