98137cc4fd3ec0cc3232ee93759510ccd16619da
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2023-06-15 17:20:00 -0600

    added ask

---
+++ src/commands/ask.ts
@@ -0,0 +1,59 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Args, Command } from '@sapphire/framework';
+import { Message } from 'discord.js';
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+	apiKey: process.env.OPENAI_API_KEY
+});
+const openai = new OpenAIApi(configuration);
+
+@ApplyOptions<Command.Options>({
+	description: 'You can ACTUALLY ask Himbot something! So cool!',
+	options: ['prompt']
+})
+export class UserCommand extends Command {
+	// Register Chat Input and Context Menu command
+	public override registerApplicationCommands(registry: Command.Registry) {
+		registry.registerChatInputCommand((builder) =>
+			builder //
+				.setName(this.name)
+				.setDescription(this.description)
+				.addStringOption((option) =>
+					option.setName('prompt').setDescription('You can ACTUALLY ask Himbot something! So cool!').setRequired(true)
+				)
+		);
+	}
+
+	// Message command
+	public async messageRun(message: Message, args: Args) {
+		return this.ask(message, args.getOption('prompt') || message.content.split('!wryna ')[1]);
+	}
+
+	// Chat Input (slash) command
+	public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+		return this.ask(interaction, interaction.options.getString('prompt') || 'NOTHING');
+	}
+
+	private async ask(interactionOrMessage: Message | Command.ChatInputCommandInteraction | Command.ContextMenuCommandInteraction, prompt: string) {
+		const askMessage =
+			interactionOrMessage instanceof Message
+				? await interactionOrMessage.channel.send({ content: '🤔 Thinking... 🤔' })
+				: await interactionOrMessage.reply({ content: '🤔 Thinking... 🤔', fetchReply: true });
+
+		const chatCompletion = await openai.createChatCompletion({
+			model: 'gpt-3.5-turbo',
+			messages: [{ role: 'user', content: prompt }]
+		});
+
+		const content = chatCompletion.data.choices[0].message?.content || 'ERROR!';
+
+		if (interactionOrMessage instanceof Message) {
+			return askMessage.edit({ content });
+		}
+
+		return interactionOrMessage.editReply({
+			content: content
+		});
+	}
+}
