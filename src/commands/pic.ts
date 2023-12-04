14ee6edae6e98cb8a96b164d63431b7222d9168e
atridadl <me@atri.dad>
2023-12-03 18:00:23 -0700

    wowee

--- src/commands/pic.ts
+++ src/commands/pic.ts
@@ -42,6 +42,8 @@ export class PicCommand extends Command {
 				width: 1024,
 				height: 1024,
 				prompt,
+				negative_prompt:
+					'out of frame, lowres, text, error, cropped, worst quality, low quality, jpeg artifacts, duplicate, morbid, mutilated, out of frame, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, username, watermark, signature.',
 				disable_safety_checker: true,
 				refine: 'expert_ensemble_refiner',
 				scheduler: 'KarrasDPM',
