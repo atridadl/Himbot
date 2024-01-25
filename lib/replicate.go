939710133d415a616e6dfe51e6bda2b5435d5a55
Atridad Lahiji <me@atri.dad>
2024-01-25 10:23:18 -0700

    Update replicate.go

--- lib/replicate.go
+++ lib/replicate.go
@@ -27,7 +27,7 @@ func ReplicateTextGeneration(prompt string) (string, error) {
 		Events: []replicate.WebhookEventType{"start", "completed"},
 	}
 
-	prediction, predictionError := client.Run(context.Background(), "meta/llama-2-70b-chat:2d19859030ff705a87c746f7e96eea03aefb71f166725aee39692f1476566d48", input, &webhook)
+	prediction, predictionError := client.Run(context.Background(), "mistralai/mistral-7b-instruct-v0.2:79052a3adbba8116ebc6697dcba67ad0d58feff23e7aeb2f103fc9aa545f9269", input, &webhook)
 
 	if predictionError != nil {
 		return "", predictionError
