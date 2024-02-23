a9f2f429876d527861b8b72e561ff743fb7d94b8
Atridad Lahiji <me@atri.dad>
2024-02-23 13:52:15 -0700

    Back to OpenAI for cost LOL

--- main.go
+++ main.go
@@ -32,17 +32,6 @@ var commands = []api.CreateCommandData{
 		},
 	},
 	{
-		Name:        "code",
-		Description: "Ask Himbot programming questions! Cooldown: 2 Minutes.",
-		Options: []discord.CommandOption{
-			&discord.StringOption{
-				OptionName:  "prompt",
-				Description: "The prompt to send to Himbot.",
-				Required:    true,
-			},
-		},
-	},
-	{
 		Name:        "pic",
 		Description: "Generate an image! Cooldown: 1 Minute.",
 		Options: []discord.CommandOption{
@@ -107,7 +96,6 @@ func newHandler(s *state.State) *handler {
 	h.Use(cmdroute.Deferrable(s, cmdroute.DeferOpts{}))
 	h.AddFunc("ping", command.Ping)
 	h.AddFunc("ask", command.Ask)
-	h.AddFunc("code", command.Code)
 	h.AddFunc("pic", command.Pic)
 	h.AddFunc("hs", command.HS)
 
